bae3663b1bcc8fd40314410668ec0a709b04cf9f
Jagdamba Tripathi <jagdtri2003@gmail.com>
2025-04-19 23:09:11 +0530

    Updated Backend LINK

--- src/socket/socket.js
+++ src/socket/socket.js
@@ -1,3 +1,3 @@
 import { io } from "socket.io-client";
 
-export const socket = io('https://devsync-server.vercel.app',{reconnectionAttempts:3})
+export const socket = io('https://devsync-server-production.up.railway.app/',{reconnectionAttempts:3})
